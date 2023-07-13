8884f6315196b850c6e3f1e401364df6d76ca30a
avincigu <arthur.vinciguerra@ens-lyon.fr>
2023-07-13 16:50:57 +0200

    improved ln function and added variants

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# scrypto-developer-toolkit
+# Scrypto Developer Toolkit
+This library aims at providing open source tools that are missing from Scrypto.
+These include:  
+- [ ] Maths library
+- [ ] Lazing loading data structures
+- [ ] Better testing engine
--- maths/src/logarithm.rs
+++ maths/src/logarithm.rs
@@ -1,14 +1,20 @@
-use radix_engine::types::{BnumI256, Decimal};
+use radix_engine::types::{BnumI256, BnumU256, Decimal};
 use crate::exponential::Exponential;
 
-pub const EULER_CONST: Decimal = Decimal(BnumI256::from_digits([2718281828459045226, 0, 0, 0]));
+pub const LN_2: Decimal = Decimal(BnumI256::from_digits([693147180559945309, 0, 0, 0]));
+pub const LN_10: Decimal = Decimal(BnumI256::from_digits([2302585092994045684, 0, 0, 0]));
+// Next power of two for the BunmU256 representation of the Decimal 1
+pub const NEXT_POWER_OF_TWO_FOR_ONE: BnumU256 = BnumU256::from_digits([1152921504606846976, 0, 0, 0]);
 
 pub trait Logarithm {
     fn ln(self) -> Self;
+    fn log2(self) -> Self;
+    fn log10(self) -> Self;
+    fn lob_base(self, base: Decimal) -> Self;
 }
 
 impl Logarithm for Decimal {
-    /// Returns the logarithm of a [`Decimal`].
+    /// Returns the natural logarithm of a [`Decimal`].
     ///
     /// The Taylor expansion of ln converges too slowly, so it is better to compute ln(y) using
     /// Halley’s method. It does it by computing the sequence x_n defined by induction:
@@ -17,41 +23,50 @@ impl Logarithm for Decimal {
     fn ln(self) -> Self {
         assert!(self.is_positive(), "Logarithm is only defined for positive numbers");
 
-        // Because, exp overflows very quickly, we rewrite y = a*e^n with 0<a<=1
-        // Therefore, ln(y) = ln(a) + n
-        let mut n = 0;
-        let mut value = self.clone();
-        while value > EULER_CONST {
-            value = value / EULER_CONST;
-            n +=1 ;
-        }
-        // Start with an arbitrary number as the first guess
-        let mut result = value / Decimal::from(2u8);
+        // Because, exp overflows very quickly, we rewrite y = 2^n(1 + x) with 0<a<=1
+        // Therefore, ln(y) = ln(1+x) + n*ln(2)
+        let self_256 = BnumU256::try_from(self.0).unwrap();
 
-        // Too small to represent, so we start with self
-        // Future iterations could actually avoid using a decimal altogether and use a buffered
-        // vector, only combining back into a decimal on return
-        if result.is_zero() {
-            result = value;
-        }
+        let pow_two = self_256.next_power_of_two()/NEXT_POWER_OF_TWO_FOR_ONE;
+        let n = pow_two.0.ilog2() as u32;
+
+        let initial_value = self / Decimal::try_from(pow_two).unwrap();
+
+        let mut result = initial_value;
         let mut last = result + Decimal::ONE;
 
         // Keep going while last and result are not equal ie. their difference is > 10^-18
         while last != result {
             last = result;
             let exp_last = last.exp();
-            result = last + (value - exp_last)/(value + exp_last)*2;
+            result = last + (initial_value - exp_last)/(initial_value + exp_last)*2;
         }
 
-        result + Decimal::from(n)
+        result + Decimal::from(n)*LN_2
+    }
+
+    /// Returns the binary logarithm of a [`Decimal`].
+    fn log2(self) -> Self {
+        self.ln()/LN_2
+    }
+
+    /// Returns the decimal logarithm of a [`Decimal`].
+    fn log10(self) -> Self {
+        self.ln()/LN_10
+    }
+
+    /// Returns the logarithm of a [`Decimal`] in a given base.
+    fn lob_base(self, base: Decimal) -> Self {
+        self.ln()/base.ln()
     }
 }
 
 #[cfg(test)]
 mod test_ln{
     use radix_engine::types::{dec, Decimal};
-    use crate::logarithm::{EULER_CONST, Logarithm};
+    use crate::logarithm::{Logarithm};
     use crate::{RELATIVE_PRECISION};
+    use crate::exponential::Exponential;
 
     #[test]
     #[should_panic]
@@ -70,6 +85,7 @@ mod test_ln{
     #[test]
     fn test_ln_1()
     {
+        println!("{}", Decimal::ONE.ln());
         assert!(Decimal::ONE.ln().abs() <= RELATIVE_PRECISION)
     }
 
@@ -83,7 +99,7 @@ mod test_ln{
     #[test]
     fn test_ln_e()
     {
-        let rel_prec = (EULER_CONST.ln() - Decimal::one()).abs();
+        let rel_prec = (Decimal::ONE.exp().ln() - Decimal::ONE).abs();
         assert!(rel_prec < RELATIVE_PRECISION);
     }
 
